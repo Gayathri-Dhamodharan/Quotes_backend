78c3ce7b32724a123ab2ae951d29c70b2a62ac8c
Gayathri Dhamodharan <your-github-email@gmail.com>
2026-01-09 17:44:29 +0530

    changed the cors

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
-
+app.use(cors(" "));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
