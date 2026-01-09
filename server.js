4165d9ed2078dff4b5ddd64ced875e86efd1f7c7
Gayathri Dhamodharan <your-github-email@gmail.com>
2026-01-09 11:57:57 +0530

    remove gitignore

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
